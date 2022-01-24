cb02369225a99817c17bb0976869720ccaa3c519
ilias0201 <iskondras0@gmail.com>
2022-01-24 02:40:06 -0500

    changed pygale to matplotlib

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import opensea
 import discord
 import config
 import time
-import pygal
+import matplotlib.pyplot as plt
 from datetime import datetime
 from discord.ext import commands
 
@@ -38,53 +38,60 @@ async def combinedfloor(ctx, wallet):
     offset = 0
     floorPrice = 0
     collectionList = {}
+    totalETHPerCollection = {}
     username = None
     while listComplete == False:
         result = api.assets(owner=wallet, offset=offset)
         
         if username is None:
-            username = result['creator']['user']['username']
+            username = result['assets'][0]['owner']['user']['username']
 
         for i in result['assets']:
             if i['collection']['slug'] in collectionList:
                 floorPrice += collectionList[i['collection']['slug']]
+                totalETHPerCollection[i['collection']['slug']] += collectionList[i['collection']['slug']]
             else:
                 time.sleep(.5)     
                 collection = api.collection(collection_slug=i['collection']['slug'])['collection']
                 try:
                     floorPrice += collection['stats']['floor_price']
                     collectionList[i['collection']['slug']] = collection['stats']['floor_price']
+                    totalETHPerCollection[i['collection']['slug']] = collection['stats']['floor_price']
                 except:
                     floorPrice += 0
                     collectionList[i['collection']['slug']] = 0
-
         if len(result['assets']) < 50:
             listComplete = True
         else:
             offset += 50
+        
+
     
-    bar_chart = generate_bar_chart(collectionList, username)
+    bar_chart = generate_bar_chart(totalETHPerCollection, username)
 
     embed = discord.Embed(
         title="Combined Floor Price of {}'s NFTs".format(username),
         url="https://opensea.io/{}".format(wallet),
         description="Combined Floor Price: {} ETH".format(round(floorPrice, 4)),
         color=discord.Color.blue())
-    
-    file = discord.File(bar_chart, filename="image.svg")
-    embed.set_image(url="attachment://image.svg")
+
+    file = discord.File(bar_chart, filename="image.png")
+    embed.set_image(url="attachment://image.png")
 
     await ctx.send(file=file, embed=embed)
     
 def generate_bar_chart(collection, username):
-    bar_chart = pygal.Bar()
-    bar_chart.title = "{}'s NFT floors".format(username)
-    
-    for key, value in collection.items():
-        bar_chart.add(key, value)
-        
-    chart_name = "chart_{}_{}.svg".format(username, datetime.now())
-    bar_chart.render_to_file(chart_name)
+    plt.clf()
+    plt.bar(collection.keys(), collection.values())
+    plt.title("{}'s Total ETH Floor Value by Collection".format(username))
+    plt.xticks(
+        rotation=45, 
+        horizontalalignment='right',
+        fontweight='light',
+        fontsize='small'  
+)   
+    chart_name = "chart_{}.png".format(username)
+    plt.savefig(chart_name, bbox_inches="tight")
     
     return chart_name
     
